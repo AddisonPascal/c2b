810cb69e25267dc8c332f373ec7b5d3dff620cf5
Addison Djatschenko <addisondj@hotmail.com>
2020-03-04 10:33:32 +1100

    Fixed volume module to not reset when importing

--- modules/volume..bat
+++ modules/volume..bat
@@ -1,11 +1,11 @@
 @echo off
 rem Compiled by the c2b Compiler from c2b v0.9.5. 
-rem Module ID: 19352388
+rem Module ID: 2212516281
 if "%alreadyStarted%"=="" set traceback_callNum=0
 set alreadyStarted=true
 set esc=
 ver | findstr /c:"Version 10"
-if errorlevel 1 goto noWin1019352388
+if errorlevel 1 goto noWin102212516281
 set esc0m=[0m
 set esc1m=[1m
 set esc4m=[4m
@@ -44,8 +44,8 @@ set esc106m=[106m
 set esc107m=[107m
 set escRewrite=[F[0J
 cls
-goto startOfFile19352388
-:noWin1019352388
+goto startOfFile2212516281
+:noWin102212516281
 set esc30m=^&powershell write-host -NoNewline -fore Black 
 set esc34m=^&powershell write-host -NoNewline -fore Blue 
 set esc32m=^&powershell write-host -NoNewline -fore Green 
@@ -64,30 +64,30 @@ set esc93m=^&powershell write-host -NoNewline -fore Yellow
 set esc97m=^&powershell write-host -NoNewline -fore White 
 set esc0m=^&echo(
 cls
-goto startOfFile19352388
-:alertJS19352388
+goto startOfFile2212516281
+:alertJS2212516281
 mshta javascript:alert("%~1");close();
 exit/b
-:ifIn19352388
+:ifIn2212516281
 echo %~2 | findstr /c:%~1
 exit /b 0
-:tracing_back_19352388
-if %tracing_back%==%traceback_callNum% goto end_tracing_back_19352388
+:tracing_back_2212516281
+if %tracing_back%==%traceback_callNum% goto end_tracing_back_2212516281
 set /a tracing_back=%tracing_back%+1
 call echo %%traceback_module_%tracing_back%%% [module ID %%traceback_moduleID_%tracing_back%%%], line %%traceback_linenum_%tracing_back%%%, in %%traceback_function_%tracing_back%%%:
 call echo %%traceback_line_%tracing_back%%%
-goto tracing_back_19352388
-:end_tracing_back_19352388
+goto tracing_back_2212516281
+:end_tracing_back_2212516281
 exit /b
-:forceDelete19352388
+:forceDelete2212516281
 del %*
-if exist %* call :forceDelete19352388 %*
+if exist %* call :forceDelete2212516281 %*
 exit /b
-:eventWait19352388
-if not exist "%temp%\Event-%*" goto eventWait19352388 %*
+:eventWait2212516281
+if not exist "%temp%\Event-%*" goto eventWait2212516281 %*
 exit /b
-:startOfFile19352388
-goto endfunction119352388
+:startOfFile2212516281
+goto endfunction12212516281
 :volume.toggleMute
 set arg1=%~1
 set arg2=%~2
@@ -101,8 +101,8 @@ echo WshShell.SendKeys ""+chr(^&hAD^)^+""
 )>"%temp%\%skey_id%.vbs"
 start "" "%temp%\%skey_id%.vbs"
 exit /b
-:endfunction119352388
-goto endfunction219352388
+:endfunction12212516281
+goto endfunction22212516281
 :volume.up
 set arg1=%~1
 set arg2=%~2
@@ -116,8 +116,8 @@ echo WshShell.SendKeys ""+chr(^&hAF^)^+""
 )>"%temp%\%skey_id%.vbs"
 start "" "%temp%\%skey_id%.vbs"
 exit /b
-:endfunction219352388
-goto endfunction319352388
+:endfunction22212516281
+goto endfunction32212516281
 :volume.down
 set arg1=%~1
 set arg2=%~2
@@ -131,8 +131,8 @@ echo WshShell.SendKeys ""+chr(^&hAE^)^+""
 )>"%temp%\%skey_id%.vbs"
 start "" "%temp%\%skey_id%.vbs"
 exit /b
-:endfunction319352388
-goto endfunction419352388
+:endfunction32212516281
+goto endfunction42212516281
 :volume.mute
 set arg1=%~1
 set arg2=%~2
@@ -146,8 +146,8 @@ echo WshShell.SendKeys ""+chr(^&hAE^)^+chr(^&hAE^)^+chr(^&hAE^)^+chr(^&hAE^)^+ch
 )>"%temp%\%skey_id%.vbs"
 start "" "%temp%\%skey_id%.vbs"
 exit /b
-:endfunction419352388
-goto endfunction519352388
+:endfunction42212516281
+goto endfunction52212516281
 :volume.set
 set arg1=%~1
 set arg2=%~2
@@ -158,37 +158,29 @@ set /a traceback_callNum=%traceback_callNum%+1
 set traceback_line_%traceback_callNum%=$volume.mute
 set traceback_linenum_%traceback_callNum%=14
 set traceback_module_%traceback_callNum%=volume
-set traceback_moduleID_%traceback_callNum%=19352388
+set traceback_moduleID_%traceback_callNum%=2212516281
 set traceback_function_%traceback_callNum%=volume.set
 call :volume.mute
 set /a traceback_callNum=%traceback_callNum%-1
 set /a upAmount=%~1/2
 set repeatCounting01=-1
-call :while0119352388
-goto afterwhile0119352388
-:while0119352388
+call :while012212516281
+goto afterwhile012212516281
+:while012212516281
 set /a repeatCounting01=%repeatCounting01%+1
-if %repeatCounting01% LSS %upAmount% call :whiling0119352388
-goto afterwhile0119352388
+if %repeatCounting01% LSS %upAmount% call :whiling012212516281
+goto afterwhile012212516281
 exit /b
-:whiling0119352388
+:whiling012212516281
 set /a traceback_callNum=%traceback_callNum%+1
 set traceback_line_%traceback_callNum%=$volume.up
 set traceback_linenum_%traceback_callNum%=17
 set traceback_module_%traceback_callNum%=volume
-set traceback_moduleID_%traceback_callNum%=19352388
+set traceback_moduleID_%traceback_callNum%=2212516281
 set traceback_function_%traceback_callNum%=volume.set
 call :volume.up
 set /a traceback_callNum=%traceback_callNum%-1
-goto while0119352388
-:afterwhile0119352388
+goto while012212516281
+:afterwhile012212516281
 exit /b
-:endfunction519352388
-set /a traceback_callNum=%traceback_callNum%+1
-set traceback_line_%traceback_callNum%=$volume.set 50
-set traceback_linenum_%traceback_callNum%=20
-set traceback_module_%traceback_callNum%=volume
-set traceback_moduleID_%traceback_callNum%=19352388
-set traceback_function_%traceback_callNum%=[module]
-call :volume.set 50
-set /a traceback_callNum=%traceback_callNum%-1
+:endfunction52212516281
